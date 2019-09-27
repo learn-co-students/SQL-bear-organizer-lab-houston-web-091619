e304adaf83db564e3d1365a2727d9bb26ad65c25
Damini varu <daminivaru@Daminis-MacBook-Pro.local>
2019-09-27 15:19:16 -0500

    damini-varu-commit

---
+++ lib/insert.sql
@@ -0,0 +1,27 @@
+
+-- CREATE TABLE bears ( 
+--     id INTEGER PRIMARY KEY, 
+--     name TEXT, 
+--     age INTEGER, 
+--     gender TEXT, (The choices could be "Male", "Female", "Other", or "Prefer not to say")|
+--     color TEXT, 
+--     temperment TEXT, 
+--     alive INTEGER );
+
+-- Mr. Chocolate
+-- * Rowdy
+-- * Tabitha
+-- * Sergeant Brown
+-- * Melissa
+-- * Grinch
+-- * Wendy
+-- * unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Cuddly", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Black", "Crazy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabita", 8, "F", "Brown", "Kind", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Black", "Angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "Brown", "Loud", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 4, "M", "Black", "Loving", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Brown", "Sluggish", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 16, "M", "Black", "Hungry", 0);
