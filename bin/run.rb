858266d40c292137660cf7b01537ce784151c115
alexiscash <alexiscash97@rocketmail.com>
2019-09-27 11:36:51 -0500

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,12 @@
 require_relative 'environment'
 
-db = SQLite3::Database.new('../db/lab_database.db')
+db = SQLite3::Database.new(':memory:')
+# db = SQLite3::Database.new('../db/lab_database.db')
 sql_runner = SQLRunner.new(db)
 
-sql_runner.execute_sql_file
+sql_runner.execute_create_file
+sql_runner.execute_insert_file
+
+all_bears = db.execute('SELECT * FROM bears')
+
+binding.pry
